f9777bc68cc5cd1fac705f2b755e1041a1e64bc5
Brian Yeh <mrbyeh@gmail.com>
2021-01-24 18:15:43 -0800

    Light PlayerFragment refactoring  - Use onViewCreated instead of onActivityCreated  - Convert LinearLayout to ConstraintLayout

--- app/src/main/java/com/audigint/throwback/ui/PlayerFragment.kt
+++ app/src/main/java/com/audigint/throwback/ui/PlayerFragment.kt
@@ -34,8 +34,8 @@ class PlayerFragment : Fragment() {
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.viewModel = playerViewModel
 
         with (playerViewModel) {
